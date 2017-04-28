6e5d215db428e6715c1fa96d0701fa2a36db66b8
Michael Gherard <mike@spotible.com>
2017-04-28 12:03:21 -0400

    began routing, imported angular

---
+++ app/routes.js
@@ -0,0 +1,8 @@
+module.exports = function(app){
+
+  app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
+  })
+
+
+}
--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var express = require('express');
 var app = express();
 var config = require('./app/config/config');
+
+require('./app/routes')(app);
+
+app.listen(3000, function () {
+  console.log('Polly Listening on 3000')
+})
+
+module.exports = app;
